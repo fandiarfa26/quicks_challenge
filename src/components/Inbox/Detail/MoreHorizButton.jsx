5ce4f8b139133908abe93e1beddede6ff478ffaf
fandiarfa26 <arfabuma.987@gmail.com>
2022-11-02 11:04:38 +0700

    fixing more button in message

--- src/components/Inbox/Detail/MoreHorizButton.jsx
+++ src/components/Inbox/Detail/MoreHorizButton.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MdMoreHoriz} from 'react-icons/md'
 
 const MoreHorizButton = () => {
+  const [isShow, setIsShow] = useState(false)
+
+  const handleEdit = () => {
+    // edit message
+    setIsShow(false)
+  }
+
+  const handleDelete = () => {
+    // delete message
+    setIsShow(false)
+  }
+  
   return (
-    <a href="#!">
-          <MdMoreHoriz className='w-4 h-4'/>
-        </a>
+    <div className='relative'>
+      <a href="#!" onClick={() => setIsShow(!isShow)}>
+        <MdMoreHoriz className='w-4 h-4'/>
+      </a>
+      <div className={`z-20 absolute py-1 flex flex-col w-24 bg-white shadow divide-y divide-secondary-light rounded border border-secondary-light ${isShow ? '':'hidden'}`}>
+        <a href="#!" onClick={handleEdit} className='px-3 py-1 text-primary hover:bg-gray-50'>Edit</a>
+        <a href="#!" onClick={handleDelete} className='px-3 py-1 text-indicator-red hover:bg-gray-50'>Delete</a>
+      </div>
+    </div>
   )
 }
 
--- src/components/Inbox/Detail/NewMessageButton.jsx
+++ src/components/Inbox/Detail/NewMessageButton.jsx
@@ -4,11 +4,13 @@ import ScrollIntoView from 'react-scroll-into-view'
 const NewMessageButton = () => {
   return (
     
-      <ScrollIntoView selector="#new-message-text" className="sticky inset-x-0 bottom-0 flex justify-center">
-        <a href="#!" className='px-4 py-2 text-sm font-bold rounded bg-primary-light text-primary'>
-          New Message
-        </a>
-      </ScrollIntoView>
+      <div className="sticky inset-x-0 bottom-0 flex justify-center">
+        <ScrollIntoView selector="#new-message-text" className='px-4 py-2 text-sm font-bold rounded cursor-pointer bg-primary-light text-primary'>
+          
+            New Message
+          
+        </ScrollIntoView>
+      </div>
     
     
   )
